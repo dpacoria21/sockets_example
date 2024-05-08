d47452a18bb8b6b99450fbfb210a119e4af0f19f
Diego Pacori <dpacoria@unsa.edu.pe>
2024-05-08 16:08:10 -0500

    Se realizan los estilos con tailwind

--- src/public/index.js
+++ src/public/index.js
@@ -7,12 +7,28 @@ const $boxChat = document.querySelector('.chat');
 
 socket.on('get-messages', async (payload) => {
     const messages = (await axios.get('http://localhost:4000/message')).data.chatUnsa.Messages;
-    const $messages = document.querySelectorAll('li') || [];
+    const $messages = document.querySelectorAll('#message') || [];
     messages.forEach((message, i) => {
-        const li = document.createElement('li');
+        
         if (message.message !== $messages[i]?.textContent) {
-            li.innerText = message.message;
-            $boxChat.append(li);
+            $boxChat.insertAdjacentHTML('beforeend', `
+            <li class="relative rounded-md rounded-tl-none bg-blue-500 w-fit h-fit px-4 py-2 my-10 mx-3 ">
+            <p id="message" class="text-sm text-white leading-tight">
+                ${message.message}
+            </p>
+            <span 
+                class="
+                    absolute w-0 h-0 transparent
+                    right-full top-0
+                    border-r-[3.5px] border-r-blue-500
+                    border-l-[3.5px] border-l-transparent
+                    border-b-[3.5px] border-b-transparent
+                    border-t-[3.5px] border-t-blue-500
+                "
+            >
+            </span>
+        </li>
+            `);
         }
     })
 })
